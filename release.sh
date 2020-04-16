6c8b844e0045c4141933b0737fc924ba1c8d0f17
hu0950 <hu0950@nenu.edu.cn>
2020-04-16 20:36:23 +0800

    [feat]test release脚本

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# 使用方法：npm run release $1(本次上线的关键信息，若不填写，默认为执行该脚本时的日期) $2(第二个参数为所需发布版本选项)
 
 CDATE=`date +'%Y%m%d%H%M%S'`
 
+# 更新项目版本，默认patch版本
 if [ -n "`git status --porcelain`" ]; then
  echo "repo is not clean or not sync with remote yet, please commit&push first"
  exit 1
@@ -15,9 +17,12 @@ else
 fi
 
 npm version $type -m '[release] npm: @%s-$CDATE'
+echo "publish version finished"
 
 git push
+echo "push version info finished"
 
+#支持
 if [ "$2" ]
 then
   tag_name="v$1-$CDATE"
@@ -25,9 +30,8 @@ else
   tag_name="v$CDATE"
 fi
 
-echo "git tag $tag_name"
 git tag $tag_name
+echo "git tag $tag_name finished"
 
-echo "git push origin $tag_name"
 git push origin $tag_name
-
+echo "git push origin $tag_name finished"
