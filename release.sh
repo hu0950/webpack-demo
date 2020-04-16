72c2352ac6f0350b8f806a84d88f5daa82a599e0
hu0950 <hu0950@nenu.edu.cn>
2020-04-16 20:26:20 +0800

    [feat]test release脚本

--- release.sh
+++ release.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+CDATE=`date +'%Y%m%d%H%M%S'`
+
+if [ -n "`git status --porcelain`" ]; then
+ echo "repo is not clean or not sync with remote yet, please commit&push first"
+ exit 1
+fi
+
+if [ -z "$2" ]
 then
   type='patch'
 else
   type=$1
 fi
 
-npm version $type -m '[release] npm: @%s'
+npm version $type -m '[release] npm: @%s-$CDATE'
 
 git push
 
+if [ "$2" ]
+then
+  tag_name="v$1-$CDATE"
+else
+  tag_name="v$CDATE"
+fi
 
+echo "git tag $tag_name"
+git tag $tag_name
+
+echo "git push origin $tag_name"
+git push origin $tag_name
 
