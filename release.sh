e5ddbd34d460b62206ca46ddc43646b4b35493cc
hu0950 <hu0950@nenu.edu.cn>
2020-04-17 09:47:11 +0800

    [feat]test release脚本

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 使用方法：npm run release $1(本次上线的关键信息，若不填写，默认为执行该脚本时的日期) $2(第二个参数为所需发布版本选项)
 
-CDATE=`date +'%Y%m%d%H%M%S'`
+DATE=`date +'%Y%m%d%H%M%S'`
 
 # 更新项目版本，默认patch版本
 if [ -n "`git status --porcelain`" ]; then
